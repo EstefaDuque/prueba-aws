37a8e5553bbcac07f1e47566b1aea61e9f7b31da
Estefa Duque <45233614+EstefaDuque@users.noreply.github.com>
2020-05-26 11:58:10 -0500

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-phantomjs-launcher')
+
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -36,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['PhantomJS'],
     singleRun: true,
     browserDisconnectTimeout: 20000,
     browserNoActivityTimeout: 400000,
